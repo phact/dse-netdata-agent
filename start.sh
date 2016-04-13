63252376efa79de43eadd4f3085f076fa2604f01
phact <none>
2016-04-13 18:43:34 -0400

    check arg

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-java -jar target/jmxfetch-0.10.0-jar-with-dependencies.jar -c mvp.yaml -p $(($1*1000)) -r console -D . -l log -L DEBUG collect
+#!/bin/bash
+
+cd /srv/dse-netdata-agent/
+frequency=1
+if [ -n "$1"   ]; then
+    frequency=$1
+fi
+java -jar target/jmxfetch-0.10.0-jar-with-dependencies.jar -c mvp.yaml -p $(($frequency*1000)) -r console -D . -l log -L DEBUG collect
